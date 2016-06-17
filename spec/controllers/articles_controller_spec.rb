b6ee8214d11e13e010dccf07a87bad8073ec1f22
Sergey Gurjev <serg.gurjev@gmail.com>
2016-06-18 00:53:27 +0800

    add several  ArticleControler specs

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe ArticlesController do
       expect(response).to render_template :show
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save the new article in the database' do
+        expect{
+          post :create, article: attributes_for(:article)
+        }.to change(Article, :count).by(1)
+      end
+
+      it 'redirect to article#show' do
+        post :create, article: attributes_for(:article)
+        expect(response).to redirect_to article_path(assigns(:article))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the new article in the database' do
+        expect{
+          post :create, article: attributes_for(:invalid_article)
+        }.to_not change(Article, :count)
+      end
+
+      it 're-renders the :new template' do
+        post :create, article: attributes_for(:invalid_article)
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     title "RoR Tutorial"
     description "Really good RoR tutorial bla bla bla"
     body { Faker::Lorem.paragraphs(6).join(" ") }
+
+    factory :invalid_article do
+      title nil
+      description nil
+      body nil
+    end
   end
 end
