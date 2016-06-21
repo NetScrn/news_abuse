e380f9b09e8c99d6465f5622c5d41481880757de
Sergey Gurjev <serg.gurjev@gmail.com>
2016-06-21 16:17:19 +0800

    configure selenium driver and db_cleaner

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users can create article' do
-  it 'authenticated should create valid article' do
+  it 'authenticated should create valid article', js: true do
     login_as create(:user)
     visit categories_path("en")
 
@@ -9,12 +9,14 @@ RSpec.describe 'Users can create article' do
 
     fill_in "Title", with: "Ruby on Rails is awesome!"
     fill_in "Description", with: Faker::Lorem.paragraph
-    fill_in "redactor-tf", with: Faker::Lorem.paragraphs(6).join(" ")
+    body = Faker::Lorem.paragraphs(6).join(" ")
+    page.execute_script("document.getElementById('redactor-tf').value = '#{body}'");
 
     click_button "Create Article"
 
     expect(page).to have_content "Article has been successfuly created."
     expect(page).to have_content "Ruby on Rails is awesome!"
+    expect(page).to have_content body
   end
 
   it 'unauthenticated users cannot create articles' do
