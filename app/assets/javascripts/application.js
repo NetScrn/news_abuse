101227b3dbb694da96b5e438d862e8bf4c22dd9b
Sergey Gurjev <serg.gurjev@gmail.com>
2016-06-28 23:38:56 +0800

    redactor fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require redactor-rails
-//= require redactor-rails/plugins
+//= require redactor-rails.js
+//= require redactor-rails/plugins.js
 //= require turbolinks
 //= require react
 //= require react_ujs
